a92e7bb9848a243cb7b323a4db09808961397027
Donkuratsa <odi_1@mail.ru>
2015-10-20 10:37:59 +0300

    Только прикрутил авторизацию и связь с задачами

--- PU/models.py
+++ PU/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Tasks(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='tasks')
     task_des = models.CharField(max_length=200)
     task_date = models.DateTimeField('date limit', blank=True, null=True)
     task_ok = models.BooleanField(default=False)
+
--- PU/serializers.py
+++ PU/serializers.py
@@ -1,9 +1,14 @@
+from django.contrib.gis import serializers
 from rest_framework import serializers
 from PU.models import Tasks
+from django.contrib.auth.models import User
 
 class TasksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tasks
         fields = ('id','task_des','task_date','task_ok')
 
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields = ('username','email','password')
